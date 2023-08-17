c212f6e305f18cc2f1fbc7d2f692bc48a2375661
simonschoe <simon.schoelzel@gmail.com>
2023-08-17 08:48:48 +0200

    modify normalization for international firms

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ from tqdm import tqdm
 app = gr.Blocks()
 
 
-def normalize_company_names(x: str):
+def normalize_company_names(x: str, geography: str = 'us'):
     '''Clean company names and prepare for fuzzy matching'''
+    
+    if geography == 'us':
+        legal_forms = re.compile(r' (public limited company|public limited|limited|unlimited|partnership|incorporation|incorporated|corporation|plc|pbc|ltd|inc|corp|llc|lp|co|company|companies|hldgs|holdings|holding)$')
+    elif geography == 'int':
+        legal_forms = re.compile(r' (public limited company|public limited|limited|unlimited|partnership|incorporation|incorporated|corporation|plc|pbc|ltd|inc|corp|llc|lp|co|company|companies|hldgs|holdings|holding|ab|ag|as|asa|berhad|bhd|bv|cva|esp|jsc|jscb|kgaa|kpsc|ksc|kscp|nv|oyj|pcl|pt|publ|spa|sae|sa|saa|saog|se|spv|tbk)$')
+    
     # lowercasing
     x = str(x).lower().strip()
     # remove special characters
@@ -18,22 +24,21 @@ def normalize_company_names(x: str):
     x = re.sub(r' (q[1-4].*?|old|adr)$', '', x)  # SA / Compustat
     x = re.sub(r' (cl ?a|cl ?b|redh)$', '', x)  # ExecuComp
     x = re.sub(r'-old$', '', x)
-    # remove legal forms
-    x = re.sub(
-        r' (public limited company|public limited|limited|unlimited|partnership|incorporation|incorporated|corporation|plc|pbc|ltd|inc|corp|llc|lp)$',
-        '', x
-    )
+    # iteratively remove legal forms from end of string
+    while legal_forms.search(x):
+        x = re.sub(legal_forms,'', x).strip()
     # normalize other frequent error cases
-    x = re.sub(r' hldgs', ' holdings', x)
-    x = re.sub(r' co$', ' company', x)
-    x = re.sub(r'^the ', '', x)
+    if geography == 'us':
+        x = re.sub(r'^the ', '', x)
+    elif geography == 'int':
+        x = re.sub(r'^(the|pt) ', '', x)
     x = re.sub(r' and ', ' ', x)
     # remove of whitespaces
     x = x.replace(' ', '')
     return x
 
 
-def normalize_person_names(x):
+def normalize_person_names(x: str):
     '''Clean company names and prepare for fuzzy matching'''
     # remove special characters
     x = re.sub(r'[^A-Za-z0-9 ]', '', x)
@@ -55,12 +60,10 @@ def fuzzy_match(query: pd.DataFrame, q_name_norm: str,
                 db: pd.DataFrame, db_name_norm: str, db_name_id: str, db_name: str,
                 q_fy: str = None, q_qtr: str = None, db_fy: str = None, db_qtr: str = None):
     '''Fuzzy match nearest neighbours for query in (filtered) database based on Levenshtein Distance
-
     :param pd.DataFrame query:
         DataFrame from which to query
     :param str q_name_norm:
         Column name of normalized string column in query
-
     :param pd.DataFrame db:
         DataFrame in which to search for match
     :param str db_name_norm:
@@ -69,7 +72,6 @@ def fuzzy_match(query: pd.DataFrame, q_name_norm: str,
         Column name of id column in database
     :param str db_name:
         Column name of string column in database
-
     :param str q_fy:
         Column name of fiscal year column in query (for filtering)
     :param str q_qtr:
@@ -78,10 +80,8 @@ def fuzzy_match(query: pd.DataFrame, q_name_norm: str,
         Column name of fiscal year column in database (for filtering)
     :param str db_qtr:
         Column name of fiscal quarter column in database (for filtering)
-
     :return pd.Series neihbours:
         Series of tuples of the form (nearest neighbour:str, similarity score:int, gvkey:int)
-
     '''
 
     def retrieve_nn(x: pd.DataFrame, db: pd.DataFrame):
@@ -110,7 +110,7 @@ def fuzzy_match(query: pd.DataFrame, q_name_norm: str,
 
 def match(p_file, p_id, p_name, p_year, p_qtr,
           s_file, s_id, s_name, s_year, s_qtr,
-          norm_person=False, out='out', progress=gr.Progress(track_tqdm=True)):
+          normalization, out='out', progress=gr.Progress(track_tqdm=True)):
 
     try:
         # input data
@@ -130,11 +130,17 @@ def match(p_file, p_id, p_name, p_year, p_qtr,
             raise ValueError("Please input either .csv or .dta")
 
         # normalize names
-        fn_norm = normalize_person_names if norm_person == "Person" else normalize_company_names
         p_name_norm = f'{p_name}_norm'
         s_name_norm = f'{s_name}_norm'
-        pri = pri.assign(**{p_name_norm: pri[p_name].map(lambda x: fn_norm(x))})
-        sec = sec.assign(**{s_name_norm: sec[s_name].map(lambda x: fn_norm(x))})
+        if normalization == 'Firm (US)': 
+            pri = pri.assign(**{p_name_norm: pri[p_name].map(lambda x: normalize_company_names(x, geography='us'))})
+            sec = sec.assign(**{s_name_norm: sec[s_name].map(lambda x: normalize_company_names(x, geography='us'))})   
+        elif normalization == 'Firm (Int)':  
+            pri = pri.assign(**{p_name_norm: pri[p_name].map(lambda x: normalize_company_names(x, geography='int'))})
+            sec = sec.assign(**{s_name_norm: sec[s_name].map(lambda x: normalize_company_names(x, geography='int'))}) 
+        elif normalization == 'Person':
+            pri = pri.assign(**{p_name_norm: pri[p_name].map(lambda x: normalize_person_names(x))})
+            sec = sec.assign(**{s_name_norm: sec[s_name].map(lambda x: normalize_person_names(x))})
 
         # match
         pri['nn_match'], pri['nn_score'], pri[f'nn_{s_id}'], pri[f'nn_{s_name}'] = \
@@ -172,12 +178,12 @@ with app:
         with gr.Column():
             with gr.Accordion("Open Instruction Manual", open=False):
                 gr.Markdown("[README.md](https://github.com/simonschoe/fuzzy-name-match/blob/master/README.md)")
-            fn_norm = gr.Radio(label="Choose entity type", choices=["Firm", "Person"], value="Firm")
+            norm = gr.Radio(label="Choose entity type", choices=["Firm (US)", "Firm (Int)", "Person"], value="Firm (US)")
             with gr.Column():
                 compute_bt = gr.Button("Start Matching", variant='primary', scale=2)
                 stop_bt = gr.Button("Stop Program", variant='secondary', scale=1)
             res = gr.File(interactive=False, label="Merged file")
-        compute_event = compute_bt.click(fn=match, inputs=[P_FILE, P_ID, P_NAME, P_YEAR, P_QTR, S_FILE, S_ID, S_NAME, S_YEAR, S_QTR, fn_norm], outputs=[res])
+        compute_event = compute_bt.click(fn=match, inputs=[P_FILE, P_ID, P_NAME, P_YEAR, P_QTR, S_FILE, S_ID, S_NAME, S_YEAR, S_QTR, norm], outputs=[res])
         stop_bt.click(fn=None, inputs=None, outputs=None, cancels=[compute_event])
 
 app.queue(max_size=1).launch(server_name='0.0.0.0')
